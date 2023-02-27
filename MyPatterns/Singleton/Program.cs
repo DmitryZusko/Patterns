49094a7cb9e5893f9b4fff25f69941404a24db62
Dmitry <elxf123@gmail.com>
2023-02-27 17:09:42 +0200

    Add a singleton pattern implementation

---
+++ MyPatterns/Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton1 = Singleton.GetInstance("First");
+            var singleton2 = Singleton.GetInstance("Second");
+
+            Console.WriteLine($"{singleton1.GetName()}");
+            Console.WriteLine($"{singleton2.GetName()}");
+            Console.WriteLine($"{singleton1 == singleton2}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyPatterns/Singleton/Singleton.cs
@@ -0,0 +1,35 @@
+﻿namespace Singleton
+{
+    /// <summary>
+    /// Singleton might be useful when we need to create one or only one instance (or number of instances shouldn't exceed fixed number).
+    /// This is the only situation when this pattern can be used and you should think twice before implementing it.
+    /// Problems:
+    /// - singleton violates SRP;
+    /// - antipattern called «singletonism» based on unsing singleton pattern all over the places;
+    /// - works badly with multi-threading.
+    /// </summary>
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private string _name;
+        private Singleton(string name)
+        {
+            _name = name;
+        }
+
+        public static Singleton GetInstance(string name)
+        {
+            if (_instance == null)
+            {
+                _instance = new(name);
+            }
+            return _instance;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+}
